fa39a1c4787e2687266dc4464f30bfcbb6cb0cad
joqk <qianyanqk@163.com>
2015-02-06 23:59:49 +0800

    系统常量添加
    支持hbase表和一些hadoop配置的统一化

---
+++ src/main/java/org/thinkit/util/Const.java
@@ -0,0 +1,13 @@
+package org.thinkit.util;
+
+/**
+ * Created by thinkit on 2015/2/6
+ * 添加系统常量.
+ */
+public class Const {
+    //hbase的表名
+    public static final String HBASE_TABLE_NAME = "hbase.tablename";
+
+    public static final String HADOOP_MAP_SPECULATIVE_EXECUTION = "mapred.map.tasks.speculative.execution";
+    public static final String HADOOP_REDUCE_SPECULATIVE_EXECUTION = "mapred.reduce.tasks.speculative.execution";
+}
