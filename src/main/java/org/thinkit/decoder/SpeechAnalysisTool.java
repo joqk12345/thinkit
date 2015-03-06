594a6e318607156743bf5600fe02440205a54483
joqk <qianyanqk@163.com>
2015-03-06 14:41:49 +0800

    添加说话人识别
    说话人识别，
    支持新数据识别（识别，存索引）
    支持使用索引进行检索

--- src/main/java/org/thinkit/decoder/SpeechAnalysisTool.java
+++ src/main/java/org/thinkit/decoder/SpeechAnalysisTool.java
@@ -22,6 +22,10 @@ public class SpeechAnalysisTool {
     private byte[] keyworddata = null; // 关键词数据
     private String keywordresult = ""; // 关键词判断结果
     private String recognitionresult = ""; //识别结果
+    private int recognitionIndexType=3;
+    private int recognitionAudioType=1;
+
+    private int resultHeaderLength =15;//返回结果头大小
 
     private String decoderServerIp = "127.0.0.1"; // decoder所在服务器ip地址
     private int decoderServerPort = 40010; // decoder所在服务器端口号
@@ -29,27 +33,26 @@ public class SpeechAnalysisTool {
     public SpeechAnalysisTool() {
     }
 
-
-    // 做固定音频检索的服务
-    public void doFixAudioSearch(){
+    //做说话人识别for 第一次识别
+    public void doSpeakerSearchForAudioByetes(){
         //发送语音头
-        FixAudioSearchProtocal.AudioHead.Builder builder1= FixAudioSearchProtocal.AudioHead.newBuilder();
-                    builder1.setIdx(0001);
-                    Timestamp d = new Timestamp(System.currentTimeMillis());
-                    builder1.setTimeStamp(d.getTime());
-                    builder1.setVersion(1);
-                    builder1.setDatacode(1);
-                    //获取数据长度的大小
-                    builder1.setDataLen(getSounddata().length);
+        FixAudioSearchProtocal.AudioHead.Builder builder1 = FixAudioSearchProtocal.AudioHead.newBuilder();
+        builder1.setIdx(0001);
+        Timestamp d = new Timestamp(System.currentTimeMillis());
+        builder1.setTimeStamp(d.getTime());
+        builder1.setVersion(1);
+        builder1.setDatacode(recognitionAudioType);
+        //获取数据长度的大小
+        builder1.setDataLen(getSounddata().length);
 
-        FixAudioSearchProtocal.AudioHead ATSHeader =builder1.build();
+        FixAudioSearchProtocal.AudioHead ATSHeader = builder1.build();
         byte[] EncodingATSHeader = ATSHeader.toByteArray();
-        //开始进行识别
-        Socket socket=null;
-        DataOutputStream ops=null;
-        DataInputStream ips=null;
+        //开始进行说话人识别
+        Socket socket = null;
+        DataOutputStream ops = null;
+        DataInputStream ips = null;
         try {
-             socket = new Socket(decoderServerIp, decoderServerPort);
+            socket = new Socket(decoderServerIp, decoderServerPort);
 
             // 开启保持活动状态的套接字
             socket.setKeepAlive(true);
@@ -59,61 +62,63 @@ public class SpeechAnalysisTool {
             ops.write(EncodingATSHeader); // 开始识别
             ops.flush();
 
-            System.out.println("开始发送语音数据" );
+            System.out.println("开始发送语音数据");
             ops.write(getSounddata());
-            System.out.println("结束发送语音数据" );
+            System.out.println("结束发送语音数据");
 
 
             // 得到返回结果
             ips = new DataInputStream(socket.getInputStream());
 
             FixAudioSearchProtocal.ResultHead testResultHead = null;
-            FixAudioSearchProtocal.ATS_Result testResult;
+            FixAudioSearchProtocal.TIT_Result titResult;
 
             if (null != ops && null != ips) {
-                int n = 15;
-                byte buffer[] = new byte[n];
+
+                byte buffer[] = new byte[resultHeaderLength];
                 // 读取输入流
-                if ((ips.read(buffer, 0, n) != -1)) {
+                if ((ips.read(buffer, 0, resultHeaderLength) != -1)) {
                     // System.out.print(new String(buffer));
 //						System.out.println("总次数为:" + (++i)	+ asr.parserArray(buffer).toString());
                     System.out.println(testResultHead = FixAudioSearchProtocal.ResultHead.parseFrom(buffer));
                 }
                 //若果识别成功 添加结果类型
-                if(1 == testResultHead.getResultCode()){
+                if (1 == testResultHead.getResultCode()) {
                     int resultLen = testResultHead.getResultLen();
                     byte buffer1[] = new byte[resultLen];
                     //读取结果信息
                     if ((ips.read(buffer1, 0, resultLen) != -1)) {
                         //打印结果信息
-                        System.out.println(testResult = FixAudioSearchProtocal.ATS_Result.parseFrom(buffer1));
+//                        System.out.println(testResult = FixAudioSearchProtocal.ATS_Result.parseFrom(buffer1));
+                        System.out.println(titResult = FixAudioSearchProtocal.TIT_Result.parseFrom(buffer1));
                         //将结果信息 格式化输出
-                        recognitionresult= parseATSResult(FixAudioSearchProtocal.ATS_Result.parseFrom(buffer1));
+                        recognitionresult = parseTITResult(FixAudioSearchProtocal.TIT_Result.parseFrom(buffer1));
+                        indexdata=FixAudioSearchProtocal.TIT_Result.parseFrom(buffer1).getIndex().toByteArray();
                     }
                 }
                 /**
                  * 错误信息处理
-                * */
+                 * */
             }
         } catch (IOException e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             //关闭连接
-            if(socket!=null){
+            if (socket != null) {
                 try {
                     socket.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
-            if(ops!=null){
+            if (ops != null) {
                 try {
                     ops.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
-            if(ips!=null){
+            if (ips != null) {
                 try {
                     ips.close();
                 } catch (IOException e) {
@@ -123,21 +128,221 @@ public class SpeechAnalysisTool {
         }
     }
 
-   private  String parseATSResult(FixAudioSearchProtocal.ATS_Result ats_result1){
-       String ats_result=null;
+    //做说话人识别for 历史检索识别
+    public void doSpeakerSearchForAudioIndexByetes(){
+//发送语音头
+        FixAudioSearchProtocal.AudioHead.Builder builder1 = FixAudioSearchProtocal.AudioHead.newBuilder();
+        builder1.setIdx(0001);
+        Timestamp d = new Timestamp(System.currentTimeMillis());
+        builder1.setTimeStamp(d.getTime());
+        builder1.setVersion(1);
+        builder1.setDatacode(recognitionIndexType);
+        //获取数据长度的大小
+        builder1.setDataLen(getSounddata().length);
 
-       int  astResultNum=ats_result1.getResultNum();
-       List<ByteString> atsArr=ats_result1.getResultListList();
-//默认选择第一个
-       if(astResultNum>0&&atsArr.size()>0){
-           ats_result=atsArr.get(0).toString();
-       }
+        FixAudioSearchProtocal.AudioHead ATSHeader = builder1.build();
+        byte[] EncodingATSHeader = ATSHeader.toByteArray();
+        //开始进行说话人识别
+        Socket socket = null;
+        DataOutputStream ops = null;
+        DataInputStream ips = null;
+        try {
+            socket = new Socket(decoderServerIp, decoderServerPort);
 
-       if(null!=ats_result){
-           return ats_result;
-       }
-       ats_result="";
-       return ats_result;
+            // 开启保持活动状态的套接字
+            socket.setKeepAlive(true);
+            ops = new DataOutputStream(socket.getOutputStream());
+
+            //1.0开始发送识别头信息
+            ops.write(EncodingATSHeader); // 开始识别
+            ops.flush();
+
+            System.out.println("开始发送语音数据");
+            ops.write(getIndexdata());
+            System.out.println("结束发送语音数据");
+
+
+            // 得到返回结果
+            ips = new DataInputStream(socket.getInputStream());
+
+            FixAudioSearchProtocal.ResultHead testResultHead = null;
+            FixAudioSearchProtocal.TIT_Result titResult;
+
+            if (null != ops && null != ips) {
+
+                byte buffer[] = new byte[resultHeaderLength];
+                // 读取输入流
+                if ((ips.read(buffer, 0, resultHeaderLength) != -1)) {
+                    // System.out.print(new String(buffer));
+//						System.out.println("总次数为:" + (++i)	+ asr.parserArray(buffer).toString());
+                    System.out.println(testResultHead = FixAudioSearchProtocal.ResultHead.parseFrom(buffer));
+                }
+                //若果识别成功 添加结果类型
+                if (1 == testResultHead.getResultCode()) {
+                    int resultLen = testResultHead.getResultLen();
+                    byte buffer1[] = new byte[resultLen];
+                    //读取结果信息
+                    if ((ips.read(buffer1, 0, resultLen) != -1)) {
+                        //打印结果信息
+//                        System.out.println(testResult = FixAudioSearchProtocal.ATS_Result.parseFrom(buffer1));
+                        System.out.println(titResult = FixAudioSearchProtocal.TIT_Result.parseFrom(buffer1));
+                        //将结果信息 格式化输出
+                        recognitionresult = parseTITResult(FixAudioSearchProtocal.TIT_Result.parseFrom(buffer1));
+                    }
+                }else if(0 == testResultHead.getResultCode()){
+                    recognitionresult="no data";
+                    System.out.println(recognitionresult);
+                }else if(-1 == testResultHead.getResultCode()){
+                    recognitionresult="network error";
+                    System.out.println(recognitionresult);
+                }else if(-3 == testResultHead.getResultCode()){
+                    recognitionresult="engineAnalysis error";
+                    System.out.println(recognitionresult);
+                }
+                /**
+                 * 错误信息处理
+                 * */
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            //关闭连接
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (ops != null) {
+                try {
+                    ops.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (ips != null) {
+                try {
+                    ips.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    // 做固定音频检索的服务
+    public void doFixAudioSearch() {
+        //发送语音头
+        FixAudioSearchProtocal.AudioHead.Builder builder1 = FixAudioSearchProtocal.AudioHead.newBuilder();
+        builder1.setIdx(0001);
+        Timestamp d = new Timestamp(System.currentTimeMillis());
+        builder1.setTimeStamp(d.getTime());
+        builder1.setVersion(1);
+        builder1.setDatacode(1);
+        //获取数据长度的大小
+        builder1.setDataLen(getSounddata().length);
+
+        FixAudioSearchProtocal.AudioHead ATSHeader = builder1.build();
+        byte[] EncodingATSHeader = ATSHeader.toByteArray();
+        //开始进行识别
+        Socket socket = null;
+        DataOutputStream ops = null;
+        DataInputStream ips = null;
+        try {
+            socket = new Socket(decoderServerIp, decoderServerPort);
+
+            // 开启保持活动状态的套接字
+            socket.setKeepAlive(true);
+            ops = new DataOutputStream(socket.getOutputStream());
+
+            //1.0开始发送识别头信息
+            ops.write(EncodingATSHeader); // 开始识别
+            ops.flush();
+
+            System.out.println("开始发送语音数据");
+            ops.write(getSounddata());
+            System.out.println("结束发送语音数据");
+
+
+            // 得到返回结果
+            ips = new DataInputStream(socket.getInputStream());
+
+            FixAudioSearchProtocal.ResultHead testResultHead = null;
+            FixAudioSearchProtocal.ATS_Result testResult;
+
+            if (null != ops && null != ips) {
+
+                byte buffer[] = new byte[resultHeaderLength];
+                // 读取输入流
+                if ((ips.read(buffer, 0, resultHeaderLength) != -1)) {
+                    // System.out.print(new String(buffer));
+//						System.out.println("总次数为:" + (++i)	+ asr.parserArray(buffer).toString());
+                    System.out.println(testResultHead = FixAudioSearchProtocal.ResultHead.parseFrom(buffer));
+                }
+                //若果识别成功 添加结果类型
+                if (1 == testResultHead.getResultCode()) {
+                    int resultLen = testResultHead.getResultLen();
+                    byte buffer1[] = new byte[resultLen];
+                    //读取结果信息
+                    if ((ips.read(buffer1, 0, resultLen) != -1)) {
+                        //打印结果信息
+                        System.out.println(testResult = FixAudioSearchProtocal.ATS_Result.parseFrom(buffer1));
+                        //将结果信息 格式化输出
+                        recognitionresult = parseATSResult(FixAudioSearchProtocal.ATS_Result.parseFrom(buffer1));
+                    }
+                }
+                /**
+                 * 错误信息处理
+                 * */
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            //关闭连接
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (ops != null) {
+                try {
+                    ops.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (ips != null) {
+                try {
+                    ips.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private String parseTITResult(FixAudioSearchProtocal.TIT_Result tit_result){
+        String titResult = null;
+        titResult =  tit_result.getSpeaker().toString();
+        return titResult;
+    }
+
+    private String parseATSResult(FixAudioSearchProtocal.ATS_Result ats_result1) {
+        String ats_result = null;
+        int astResultNum = ats_result1.getResultNum();
+        List<ByteString> atsArr = ats_result1.getResultListList();
+        //默认选择第一个
+        if (astResultNum > 0 && atsArr.size() > 0) {
+            ats_result = atsArr.get(0).toString();
+        }
+        if (null != ats_result) {
+            return ats_result;
+        }
+        ats_result = "";
+        return ats_result;
     }
 
     public long getId() {
